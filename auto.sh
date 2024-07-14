3922b7b3b50d9c481db19d2fc4c5c610eaa470d1
Minuring <mw9505@gmail.com>
2024-07-15 01:51:04 +0900

    Upload Build 2024년 7월 15일 월요일 01시 51분 04초 KST

--- auto.sh
+++ auto.sh
@@ -13,7 +13,13 @@ git add .
 git commit -m "Upload Build $(DATE)"
 git push
 
-ssh -i "KSSoftware_KeyPair.pem" ubuntu@ec2-3-35-141-181.ap-northeast-2.compute.amazonaws.com
-echo "Connected to Server using SSH"
+ssh -i "KSSoftware_KeyPair.pem" ubuntu@ec2-3-35-141-181.ap-northeast-2.compute.amazonaws.com <<\EOF
 
+echo "Connected to Server using SSH"
 bash server_auto.sh
+cd /usr/local
+sudo rm -rf app
+sudo git clone https://github.com/Minuring/KSSoftware_Test_Download.git app
+java -jar app/K*.jar
+
+EOF
---
+++ test.sh
@@ -0,0 +1,3 @@
+ssh -i "KSSoftware_KeyPair.pem" ubuntu@ec2-3-35-141-181.ap-northeast-2.compute.amazonaws.com <<\EOF
+bash server_auto.sh
+EOF
