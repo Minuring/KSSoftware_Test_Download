7fda28138d37acde6694241ce84562906c9bd3f7
Minuring <mw9505@gmail.com>
2024-07-15 01:36:59 +0900

    Upload Build 2024년 7월 15일 월요일 01시 36분 59초 KST

--- auto.sh
+++ auto.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 cd ~/projects/intellij-workspace/KSSoftware
 ./gradlew bootJar
 mv build/libs/K*.jar ~/Desktop/AutoBuild
@@ -14,9 +14,9 @@ git commit -m "Upload Build $(DATE)"
 git push
 
 ssh -i "KSSoftware_KeyPair.pem" ubuntu@ec2-3-35-141-181.ap-northeast-2.compute.amazonaws.com
+echo "Connected to Server using SSH"
 
 cd /usr/local
 sudo rm -rf app
 sudo git clone https://github.com/Minuring/KSSoftware_Test_Download.git app
-cd app
-java -jar K*.jar
+java -jar app/K*.jar
